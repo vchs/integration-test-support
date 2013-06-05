e78ae265c7563b1dc34c77839065be866c3b89e5
phanle <phan.anh.le@gmail.com>
2013-06-04 22:55:23 -0700

    Created an example of how to run the integration tests

---
+++ example/example_spec.rb
@@ -0,0 +1,10 @@
+require_relative 'spec_helper'
+
+describe 'test that requires CF components', type: :integration, :components => [:nats, :ccng] do
+  include CcngClient
+
+  it 'start nats and ccng as required' do
+    component(:nats).should be
+    ccng_get('/v2/services').should be
+  end
+end
