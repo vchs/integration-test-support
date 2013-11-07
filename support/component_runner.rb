5e702384c0e558f439bd379dcfac698666a7cf8d
Felix Hu <hhu@vmware.com>
2013-11-07 13:56:34 +0800

    add sc helper for integration test
    Change-Id: Iabda38ab23e401b9bed6325adb61cfeb28b573d5

--- support/component_runner.rb
+++ support/component_runner.rb
@@ -79,11 +79,11 @@ class ComponentRunner < Struct.new(:tmp_dir, :rspec_example)
     File.expand_path(File.join(root, 'assets', file_name))
   end
 
-  def wait_for_http_ready(label, port)
+  def wait_for_http_ready(label, port, path='info')
     print "Waiting for #{label}..."
     retries = 30
     begin
-      response = client.get("http://localhost:#{port}/info")
+      response = client.get("http://localhost:#{port}/#{path}")
       raise "Failed to connect, status: #{response.status}" unless response.ok?
       puts "ready!"
     rescue
