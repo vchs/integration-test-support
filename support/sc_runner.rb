cfc209c9c3a2c31181b997bd8d380b389a3bd718
Felix Hu <hhu@vmware.com>
2013-11-07 13:56:34 +0800

    add sc helper for integration test
    Change-Id: Iabda38ab23e401b9bed6325adb61cfeb28b573d5

---
+++ support/sc_runner.rb
@@ -0,0 +1,34 @@
+require_relative 'component_runner'
+
+class ScRunner < ComponentRunner
+
+  def start
+    Dir.chdir "#{tmp_dir}/service_controller" do
+      Bundler.with_clean_env do
+        prepare_sc
+        add_pid Process.spawn "bundle exec ./bin/rails server", log_options(:sc)
+      end
+    end
+    wait_for_http_ready("SC", 3000, '')
+  end
+
+  def stop
+    super
+  ensure
+    cleanup_sc
+  end
+
+  private
+
+  def prepare_sc
+    Dir.chdir "#{tmp_dir}/service_controller" do
+      sh "bundle exec rake db:migrate"
+    end
+  end
+
+  def cleanup_sc
+    Dir.chdir "#{tmp_dir}/service_controller" do
+      sh "git reset --hard && git clean -dffx"
+    end
+  end
+end
